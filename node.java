f489bd6d8116ef92fe7cea8ca4a5574e908de5d8
Geoff10 <jukesg@cardiff.ac.uk>
2016-02-09 22:40:29 +0000

    Uploaded the skeleton node class node.java

---
+++ node.java
@@ -0,0 +1,33 @@
+public class node {
+	/*
+	Okay so this is the basic skeleton of the node class.
+	It's not 100% accuarte yet, for example not all of the return
+	types are correct and I couldn't remember what the finger table
+	type will be so please change and update them. :)
+	*/
+	private Map fingerTable;
+	public void join() {
+		//Jake and Jamie
+	}
+	public void leave() {
+		//Jammy and Chris
+	}
+	public void getFingerTable() {
+		//Jake and Jamie
+	}
+	public void findClosest() {
+		//
+	}
+	public String getNode() {
+		//Geoff
+	}
+	public boolean sendMessage(String message, int destNode) {
+		//Geoff
+		String ip = getNode(node);
+	}
+	public boolean recieveMessage(String message) {
+		//Geoff
+		System.out.println(message);
+		return true;
+	}
+}
