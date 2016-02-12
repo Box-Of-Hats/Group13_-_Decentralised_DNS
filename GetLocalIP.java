026ee2e249854d11e9321f400e1b90ec7ced33aa
JiamingKe <JiamingKe@outlook.com>
2016-02-12 17:22:03 +0000

    Update GetLocalIP.java

--- GetLocalIP.java
+++ GetLocalIP.java
@@ -2,16 +2,21 @@ import java.net.*;
 import java.util.*;
 
 public class GetLocalIP {
-	public static void main (String args[]) throws SocketException{
-		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        for (NetworkInterface netint : Collections.list(nets)) {
-            Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-        	for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-	        	if (!inetAddress.isAnyLocalAddress() && !inetAddress.isLinkLocalAddress() && !inetAddress.isLoopbackAddress() && !inetAddress.isMulticastAddress()) {
-	        		System.out.printf("%s", inetAddress);
-	        		System.out.println("This is the public address");
-	        	}
-	        }
-        }
+	public static InetAddress get(){
+		InetAddress ip = null;
+		try{
+			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+			for (NetworkInterface netint : Collections.list(nets)) {
+        			Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        			for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+	        			if (!inetAddress.isAnyLocalAddress() && !inetAddress.isLinkLocalAddress() && !inetAddress.isLoopbackAddress() && !inetAddress.isMulticastAddress()) {
+	        				ip = inetAddress;
+	        				break;
+	        			}
+	        		}
+        		}	
+		}catch(Exception e){
+		}
+		return ip;
 	}
 }
