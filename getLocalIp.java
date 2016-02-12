fe41d8a2e9064ad1d408118cddc25e28d3111dea
Jay <Jaydub.design@googlemail.com>
2016-02-12 14:32:59 +0000

    Added code to find the outward facing IP of a machine

---
+++ getLocalIp.java
@@ -0,0 +1,16 @@
+import java.net.*;
+import java.util.*;
+
+public class Test {
+	public static void main (String args[]) throws SocketException{
+		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets)) {
+            Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        	for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+	        	if (!inetAddress.isAnyLocalAddress() && !inetAddress.isLinkLocalAddress() && !inetAddress.isLoopbackAddress() && !inetAddress.isMulticastAddress()) {
+	        		System.out.printf("%s", inetAddress);
+	        		System.out.println("This is the public address");
+	        	}
+	        }
+        }
+	}
