4a2c9d6db78dcddfc78205b8b11c3f3d47c96480
JiamingKe <JiamingKe@outlook.com>
2016-02-12 14:30:33 +0000

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,24 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.IOException;
-import java.net.Socket;
+import java.io.PrintWriter;
 import java.net.Inet4Address;
+import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.HashMap;
 
-public class Client implements Runnable
-{
+public class Client {
 	private final int PORT = 3333;
-	Socket socket;
-	private ObjectInputStream in;
-	private ObjectOutputStream out;
-	public String ip;
-
-	public void run()
-	{
-
-	}
-
+	private String ip;
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	
 	public Client()
 	{
 		try
@@ -27,115 +23,54 @@ public class Client implements Runnable
 		}
 		catch (UnknownHostException e)
 		{
-			System.out.println(e);
+			System.out.println(e.getMessage());
 		}
 	}
-
-	private void connectToServer(String ip)
+	
+	public void connectToServer(String ip)
 	{
 		try
 		{
 			socket = new Socket(ip, PORT);
+			out = new PrintWriter(socket.getOutputStream(), true);
+    		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		}
 		catch (IOException e)
 		{
-			System.out.println(e);
-			return;
-		}
-
-		try
-		{
-			in = new ObjectInputStream(socket.getInputStream());
-			out = new ObjectOutputStream(socket.getOutputStream());
-		}
-		catch (IOException e)
-		{
-			System.out.println(e);
+			System.out.println(e.getMessage());
 		}
 	}
-
-	private void disconnect()
+	
+	public void pushMessageToServer(String string){
+		try{
+			out.println(string);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String pullMessageFromServer(){
+		String string = null;
+		try{
+			string = in.readLine();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return string;
+	}
+	
+	public void disconnect()
 	{
 		try
 		{
+			in.close();
+			out.close();
 			socket.close();
+			System.out.println("Socket is closed.");
 		}
 		catch (IOException e)
 		{
-			System.out.println(e);
+			System.out.println(e.getMessage());
 		}
-	}
-
-	// Connects to computer with corresponding IP, sends a message to them and then closes the connection
-	public <T> void sendMessage(String ip, T message)
-	{
-		connectToServer(ip);
-
-		try
-		{
-			// Writes the message to the output stream
-			out.writeObject(new Message<T>(MessageType.Plain, message, this.ip));
-		}
-		catch (IOException e)
-		{
-			System.out.println(e);
-		}
-
-		disconnect();
-	}
-
-	public void sendMessage(String ip, MessageType type)
-	{
-		connectToServer(ip);
-
-		try
-		{
-			// Writes the message to the output stream
-			out.writeObject(new Message(type, this.ip));
-		}
-		catch (IOException e)
-		{
-			System.out.println(e);
-		}
-
-		disconnect();
-	}
-
-	public <T> void sendMessage(String ip, MessageType type, T message)
-	{
-		connectToServer(ip);
-
-		try
-		{
-			// Writes the message to the output stream
-			out.writeObject(new Message<T>(type, message, this.ip));
-		}
-		catch (IOException e)
-		{
-			System.out.println(e);
-		}
-
-		disconnect();
-	}
-
-	public void requestFingerTable(String receiverIp)
-	{
-		// Asks destination node for complete finger table and passes it back.
-		sendMessage(receiverIp, MessageType.RequestFingerTable);
-	}
-
-	public void sendFingerTable(String receiverIp, HashMap table)
-	{
-		sendMessage(receiverIp, MessageType.ReceiveFingerTable, table);
-	}
-
-	public void requestNodeIp(String receiverIp, int nodeId)
-	{
-		sendMessage(receiverIp, MessageType.RequestNodeIp);
-	}
-
-	public void sendNodeIp(String receiverIp, String ip);
-	{
-		sendMessage(receiverIp, MessageType.ReceiveNodeIp, ip);
 	}
 }
