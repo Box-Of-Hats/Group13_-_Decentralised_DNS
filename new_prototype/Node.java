002c7ea3e811e0e433f0730a295b3911cde85b3c
Vault-hunter <ja1ke@hotmail.co.uk>
2016-03-01 10:04:59 +0000

    Added Finger template. Added variables to Node class

--- new_prototype/Node.java
+++ new_prototype/Node.java
@@ -1,4 +1,14 @@
 public class Node{
+    /*
+    guid: the unique identifier of the node
+    ip: the ip of the node
+    Finger[]: an array of finger objects
+    predecessor: the directly preceding node
+    */
+
+    private int guid;;
+    private Finger[] finger;
+    private Node predecessor;
 
     public Node(){
 
