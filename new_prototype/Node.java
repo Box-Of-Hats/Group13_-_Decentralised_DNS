03b56bcedeb873b938a3ca1eda0b8eb14a80d837
Box-Of-Hats <ja1ke@hotmail.co.uk>
2016-03-02 10:50:05 +0000

    Added method comments with pseudocode for implemented methods

--- new_prototype/Node.java
+++ new_prototype/Node.java
@@ -53,29 +53,69 @@ public class Node{
 
     public void join(String bootstrapNodeIp){
         //This method requires network code to contact other nodes
+        /*
+        initFingerTable(bootstrapNodeIp)
+        updateOthers()
+        */
     }
 
     public void initFingerTable(String bootstrapNodeIp){
         //This method requires a lot of network code
+        /*
+        //n' is the node represented by bootstrapNodeIp
+        n.finger[0] = n'.findSuccessor(finger[i].start)
+        // This line contacts the bootstrap node and asks it to run its findSuccessor code using the id of the current node
+        // This finds the succesor node of the current node, to do this the server would simply need to call its node's
+        // findSuccessor method
+        predecessor = successor.predecessor // this requires the server to have some method to retrieve its node's predecessor
+        successor.predecessor = n // This requires the server to have some method to set its node's predecessor
+        for i = 0 to m - 1;
+            if (finger[i + 1].start is between [n, finger[i].node]):
+                finger[i + 1].node = finger[i].node
+            else:
+                finger[i + 1].node = n'.findSucessor(finger[i + 1].start)
+            */
     }
 
     public void updateOthers(){
         //This method requires network code to update other nodes finger tables
+        /*
+        for i = 1 to m:
+            p = findPredecessor(n - 2^(i - 1))
+            p.updateFingerTable(n, i)//This requires the server to call its node updateFingerTable method with the given arguments
+        */
     }
 
     public void updateFingerTable(int s, int i){
         //This method requires network code to step back along the network 
-        //!!! Im unsure of the expected types of s and i.
+        //!!! Im unsure of the expected types of s and i. Using int for now, as a placeholder.
+        /*
+        if (s is between [n, finger[i].node]):
+        finger[i].node = s;
+        p = n.predecessor;
+        p.updateFingerTable(s, i)//This requires the server to call its node updateFingerTable method with the given arguments
+        */
     }
 
     public Node findSuccessor(int id){
         //This requires network code to retrieve the sucessor of the target node
+        /*
+        n' = findPredecessor(id)
+        return n'.sucessor()//The contacted server should then retrieve the finger[0].node field of its nod
+        */
         return this; //!! PLACEHOLDER !!
     }
 
     public Node findPredecessor(int id){
         //This requires network code
+        /*
+        n' = n//where n is the current node
+        while (id is not between [n', n'.successor]):
+            n' = n'.closestPrecedingFinger(id)//This asks the appropriate server to call its node's closestPrecedingFingerMethod
+        return n'
+        */
         return this; //!! PLACEHOLDER !!
+
     }
 
     public Finger closestPrecedingFinger(int key){
@@ -91,5 +131,3 @@ public class Node{
     }
 
 }
-
-
