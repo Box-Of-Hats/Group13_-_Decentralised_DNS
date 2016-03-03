d4dc32370c7efc17273c6b817d8a03932e043f6e
JiamingKe <JiamingKe@outlook.com>
2016-03-03 11:46:13 +0000

    Update NodeClient.java

--- new_prototype/NodeClient.java
+++ new_prototype/NodeClient.java
@@ -2,7 +2,7 @@ import java.net.InetAddress;
 
 public abstract class NodeClient{
     
-   public abstract boolean connectToServer(InetAddress ip);
+   public abstract boolean connectToServer(String ip);
    
    public abstract void disconnect();
    
