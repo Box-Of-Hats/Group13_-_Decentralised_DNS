38dd708f268e651a335c374e9c45ff2f7e1a767d
Jay <Jaydub.design@googlemail.com>
2016-04-24 20:01:50 +0100

    sadf

--- new_prototype/Node.java
+++ new_prototype/Node.java
@@ -270,14 +270,14 @@ public class Node{
             successorId = successorId + MAXSIZE;
         
         //Repeats until it finds a node that should have the ID as its successor
-        while (!((nodeId <= id) && (id < successorId))) {
+        while (!((nodeId <= id) && (id <= successorId))) {
             if (currentNode) {
                 if (id > MAXSIZE)
                     id = id - MAXSIZE;
                 node = closestPrecedingFinger(id);
                 currentNode = false;
             } else if (nodeId == id) {
-                return node;
+                node = new FingeredNode(ip ,guid);
             } else {
                 if (id > MAXSIZE)
                     id = id - MAXSIZE;
