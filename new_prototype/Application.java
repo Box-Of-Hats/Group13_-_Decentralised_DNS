e7b1aaca7028a38b8440b843ffb1986f76f8455b
Jake Berg <C:\Users\ja1ke\AppData\Roaming\The Bat!>
2016-04-24 17:40:16 +0100

    pushed it real good

--- new_prototype/Application.java
+++ new_prototype/Application.java
@@ -1,8 +1,18 @@
 class Application{
+
     public static void main(String[] args){
-        for(int i=0; i<args.length; i++ ){
-            System.out.println(args[i]);
+
+        //Check if arguements were supplied. If not, helpful text will be displayed containing all
+        // possible commands with arguement parameters.
+        if (args.length == 0){
+            System.out.println("No arguements supplied so no actions have been taken.");
+            System.out.println("Available actions:");
+            System.out.println("\tjoin [-BootstrapIP]  :Join a network via the ip of a bootstrap node.");
         }
+        else{
+            String action = args[0];
+        }
+
     }
 
 }
