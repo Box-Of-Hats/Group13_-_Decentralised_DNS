57279b4c41e7ae934ac960bd9a5451a1ee4c19fe
Jiaming <JiamingKe@outlook.com>
2016-04-27 11:43:32 +0100

    asdds

--- new_prototype/Node.java
+++ new_prototype/Node.java
@@ -141,24 +141,13 @@ public class Node{
             point predeseccors successor to nodes successor,
             point successors predecessor to nodes predecessor
         */
+        //All URLs stored on the current node must be passed to its direct successor, and removed from node's dataStore
         FingeredNode successor = fingerTable[0].getNode();
         client.connectToServer(successor.getIp());
 
         data.forEach((url,ip) -> subQuit(url,ip));
         data.clear();
-
-        //All URLs stored on the current node must be passed to its direct successor, and removed from node's dataStore
-        // FingeredNode successor = fingerTable[0].getNode();
-        // client.connectToServer(successor.getIp());
-        // String request = "";
-        // Iterator<Map.Entry<String, String>> it = data.entrySet().iterator();
-        // while (it.hasNext()) {
-        //     Map.Entry<String,String> pair = (Map.Entry)it.next();
-        //     request = "AUD," + pair.getKey() + ";" + pair.getValue();
-        //     client.pushMessage(request);
-        //     String response = client.pullMessage();
-        // }
-        // data.clear();
+        client.disconnect();
 
         //Other nodes that may point to the current node in there finger table must now point to its successor
 
@@ -198,11 +187,7 @@ public class Node{
         */
 
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> a46c41972c50ef62ce264f4b2df6b3f7dc0322c8
     private void subQuit(String url, String ip){
         client.pushMessage("AUD," + url + ";" + ip);
         String response = client.pullMessage();
